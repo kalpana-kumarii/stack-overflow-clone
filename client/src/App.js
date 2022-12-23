317017bd61743317b8d064eea4ed1d99d80c38ec
Kalpana Kumari <kalpana0607@gmail.com>
2022-12-23 19:14:46 +0530

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import {BrowserRouter as Router} from'react-router-dom'
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+    <h1>Stack overflow clone</h1>
+
+      </Router>
+    </div>
+  );
+}
+
+export default App;
